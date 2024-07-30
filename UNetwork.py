28323555c3d1e31a327f89d8437969b03ee61c0d
Jacob Dent <murkymicrobe88@gmail.com>
2024-07-30 16:36:16 -0400

    Unet shape finished

--- UNetwork.py
+++ UNetwork.py
@@ -26,4 +26,22 @@ class UNet(nn.Module):
         self.out = nn.Conv2d(in_channels=64, out_channels=num_classes, kernel_size=1)
 
     def forward(self, x):
+        # Forward for the left side of the U-shape
         down_1, p1 = self.down_conv_1(x)
+        down_2, p2 = self.down_conv_2(p1)
+        down_3, p3 = self.down_conv_3(p2)
+        down_4, p4 = self.down_conv_4(p3)
+
+        # Forward for the bottle neck of the U-shape
+        neck = self.bottle_neck(p4)
+
+        # Forward for the right side of the U-shape
+        up_1 = self.up_conv_1(neck, down_4)
+        up_2 = self.up_conv_2(up_1, down_3)
+        up_3 = self.up_conv_3(up_2, down_2)
+        up_4 = self.up_conv_4(up_3, down_1)
+
+        # Output
+        out = self.out(up_4)
+        return out
+
